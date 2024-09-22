791bb0f72a0c05a325baf0abead179048335e7e6
nathnael-chilot <chilotnathnael@gmail.com>
2024-09-22 16:39:28 +0300

    creat project and navigation bar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md w-full">
+      <div className="container mx-auto px-4 flex items-center justify-between h-[75px] max-w-[1440px]">
+        {/* Logo Section */}
+        <div className="flex items-center">
+          <img src="/logo.png" alt="Pizza Logo" className="h-10 w-10 mr-2" />
+          <span className="text-xl font-bold">Pizza</span>
+        </div>
+
+        {/* Navigation Links */}
+        <ul
+          className="w-[605px] h-[36px] top-[24px] left-[392px] 
+          flex items-center justify-between gap-0 opacity-100"
+        >
+          <li>
+            <a
+              href="#home"
+              className="font-inter text-[25px] font-bold leading-[36.17px] tracking-[0.03em] text-left text-[#FF8100] text-gray-700 hover:text-[#FF8100]"
+            >
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="#order"
+              className="w-[86px] h-[36px] gap-0 opacity-100 font-inter text-[25px] font-medium leading-[36.17px] tracking-[0.03em] text-left text-[#16120DBF] hover:text-[#FF8100]"
+            >
+              Orders
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="#order"
+              className="w-[144px] h-[36px] gap-0 opacity-100 font-inter text-[25px] font-medium leading-[36.17px] tracking-[0.03em] text-left text-[#16120DBF] hover:text-[#FF8100]"
+            >
+              who we are
+            </a>
+          </li>
+        </ul>
+
+        {/* Register Button */}
+        <div>
+          <button
+            className="w-[168px] h-[56px] top-[14px] left-[1244px] 
+  px-[30px] py-[10px] gap-0 rounded-[5px] bg-[#FF890F] opacity-100"
+          >
+            <span className="w-[108px] h-[36px] font-inter text-[25px] font-bold leading-[36.17px] tracking-[0.03em] text-left text-white">
+              Register
+            </span>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
